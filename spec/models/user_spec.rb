0fcd6675b56da9a9e7d2d82656705aa0301d3d00
E-G-Lima <elaineguedes2120@gmail.com>
2022-08-15 15:56:23 -0300

    configuração da tela de login dos usuários

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+=begin
+  it "user is valid" do
+    user = User.new(name: "Elaine", 
+    email: "elaine@teste.com", 
+    phone: "85999999999", 
+    avatar: fixture_file_upload(Rails.root.join("spec/support/test-avatar.png", 'image/png')),
+    password: "123456")
+
+    expect(user).to be_valid
+  end
+
+  it "admin is not valid" do
+  
+  end
+=end
+end
